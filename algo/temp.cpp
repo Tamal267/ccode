bc024aff5c7909658dffafd9a052a284138cd652
Syed Tamal <68176996+Tamal267@users.noreply.github.com>
2024-10-22 03:26:27 +0600

    Create temp.cpp

---
+++ algo/temp.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define i64 long long
+#ifdef ONPC
+#include "mydebug.h"
+#else
+#define debug(...) 1
+#endif
+
+void solve() {
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n = 1;
+  cin >> n;
+  for (int i = 1; i <= n; i++) {
+//    cout << "Case #" << i << ": ";
+    solve();
+  }
+}
