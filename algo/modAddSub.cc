7b4f535c994458801aab12544da93b54f4b35a86
Tamal267 <syedtamal@gmail.com>
2025-09-01 18:03:15 +0600

    modAddSub

--- algo/modAddSub.cc
+++ algo/modAddSub.cc
@@ -1,14 +1,14 @@
 int add(int x, int y) {
-     x += y;
-     if (x >= mod) x -= mod;
-     if (x < 0) x += mod;
-     return x;
+	x += y;
+	if (x >= mod) x -= mod;
+	if (x < 0) x += mod;
+	return x;
 }
- 
+
 int mul(int x, int y) {
-    return x * 1LL * y % mod;
+	return x * 1LL * y % mod;
 }
 
 int divide(int x, int y) {
-    return mul(x, power(y, mod - 2));
+	return mul(x, power(y, mod - 2));
 }
