ae02a79865e3d97b7336018b0429b27f90ef41d0
Tamal267 <syedtamal@gmail.com>
2025-09-28 10:59:50 +0600

    suffix array up

--- algo/suffix_array.cc
+++ algo/suffix_array.cc
@@ -1,123 +1,78 @@
-class SuffixArray {
-	private:
-		string s;
-		int n;
-		vector<int> sa, rank, lcp;
-
-		void build_sa() {
-			sa.resize(n);
-			vector<int> c(n), cnt(n);
-			for (int i = 0; i < n; i++) sa[i] = i;
-			sort(sa.begin(), sa.end(), [&](int a, int b) {
-					return s[a] < s[b];
-					});
-
-			c[sa[0]] = 0;
+struct SuffixArray {
+	vector<int> p, c, rank, lcp;
+	vector<vector<int>> st;
+	SuffixArray(string const& s) {
+		build_suffix(s + char(1));
+		build_rank(p.size());
+		build_lcp(s + char(1));
+		build_sparse_table(lcp.size());
+	}
+	void build_suffix(string const& s) {
+		int n = s.size();
+		const int MX_ASCII = 256;
+		vector<int> cnt(max(MX_ASCII, n), 0);
+		p.resize(n); c.resize(n);
+		for (int i = 0; i < n; i++) cnt[s[i]]++;
+		for (int i=1; i<MX_ASCII; i++) cnt[i]+=cnt[i-1];
+		for (int i = 0; i < n; i++) p[--cnt[s[i]]] = i;
+		c[p[0]] = 0;
+		int classes = 1;
+		for (int i = 1; i < n; i++) {
+			if (s[p[i]] != s[p[i-1]]) classes++;
+			c[p[i]] = classes - 1;
+		}
+		vector<int> pn(n), cn(n);
+		for (int h = 0; (1 << h) < n; ++h) {
+			for (int i = 0; i < n; i++) {
+				pn[i] = p[i] - (1 << h);
+				if (pn[i] < 0) pn[i] += n;
+			}
+			fill(cnt.begin(), cnt.begin() + classes, 0);
+			for (int i = 0; i < n; i++) cnt[c[pn[i]]]++;
+			for (int i=1; i<classes; i++) cnt[i]+=cnt[i-1];
+			for (int i=n-1;i>=0;i--) p[--cnt[c[pn[i]]]]=pn[i];
+			cn[p[0]] = 0; classes = 1;
 			for (int i = 1; i < n; i++) {
-				c[sa[i]] = c[sa[i-1]] + (s[sa[i]] != s[sa[i-1]]);
+				pair<int, int> cur = {c[p[i]], c[(p[i] + (1 << h)) % n]};
+				pair<int, int> prev = {c[p[i-1]], c[(p[i-1] + (1 << h)) % n]};
+				if (cur != prev) ++classes;
+				cn[p[i]] = classes - 1;
 			}
-			for (int k = 0; (1 << k) < n; k++) {
-				for (int i = 0; i < n; i++) {
-					sa[i] = (sa[i] - (1 << k) + n) % n;
-				}
-				fill(cnt.begin(), cnt.end(), 0);
-				for (int i = 0; i < n; i++) {
-					cnt[c[sa[i]]]++;
-				}
-				for (int i = 1; i < n; i++) {
-					cnt[i] += cnt[i-1];
-				}
-				vector<int> sa_new(n);
-				for (int i = n-1; i >= 0; i--) {
-					sa_new[--cnt[c[sa[i]]]] = sa[i];
-				}
-				sa = sa_new;
-				vector<int> c_new(n);
-				c_new[sa[0]] = 0;
-				for (int i = 1; i < n; i++) {
-					pair<int, int> prev = {c[sa[i-1]], c[(sa[i-1] + (1 << k)) % n]};
-					pair<int, int> curr = {c[sa[i]], c[(sa[i] + (1 << k)) % n]};
-					c_new[sa[i]] = c_new[sa[i-1]] + (prev != curr);
-				}
-				c = c_new;
-			}
+			c.swap(cn);
 		}
-		// lcp(sa[i], sa[i + 1])
-		void build_lcp() {
-			rank.resize(n);
-			lcp.resize(n);
-			for (int i = 0; i < n; i++) {
-				rank[sa[i]] = i;
+	}
+	void build_rank(int n) {
+		rank.resize(n, 0);
+		for (int i = 0; i < n; i++) rank[p[i]] = i;
+	}
+	void build_lcp(string const& s) {
+		int n = s.size(), k = 0;
+		lcp.resize(n - 1, 0);
+		for (int i = 0; i < n; i++) {
+			if (rank[i] == n - 1) {
+				k = 0;
+				continue;
 			}
-			int k = 0;
-			for (int i = 0; i < n; i++) {
-				if (rank[i] == n-1) {
-					k = 0;
-					continue;
-				}
-				int j = sa[rank[i] + 1];
-				while (i + k < n && j + k < n && s[i+k] == s[j+k]) {
-					k++;
-				}
-				lcp[rank[i]] = k;
-				if (k > 0) k--;
-			}
+			int j = p[rank[i] + 1];
+			while (i + k < n && j + k < n && s[i+k] == s[j+k])
+				k++;
+			lcp[rank[i]] = k;
+			if (k) k--;
 		}
-	public:
-		SuffixArray(const string& str) : s(str) {
-			n = s.size();
-			build_sa();
-			build_lcp();
-		}
-
-		const vector<int>& get_sa() const { return sa; }
-		const vector<int>& get_lcp() const { return lcp; }
-		const vector<int>& get_rank() const { return rank; }
-
-		int find(const string& pattern) {
-			int m = pattern.size();
-			int l = 0, r = n - 1;
-			while (l <= r) {
-				int mid = (l + r) / 2;
-				int cmp = s.compare(sa[mid], min(n - sa[mid], m), pattern);
-				if (cmp == 0) return sa[mid];
-				else if (cmp < 0) l = mid + 1;
-				else r = mid - 1;
-			}
-			return -1;
-		}
-		int count_occurrences(const string& pattern) {
-			int m = pattern.size();
-			// Find lower bound
-			int low = 0, high = n - 1;
-			while (low <= high) {
-				int mid = (low + high) / 2;
-				if (s.compare(sa[mid], min(n - sa[mid], m), pattern) < 0) {
-					low = mid + 1;
-				} else {
-					high = mid - 1;
-				}
-			}
-			int first = low;
-			// Find upper bound
-			low = 0, high = n - 1;
-			while (low <= high) {
-				int mid = (low + high) / 2;
-				if (s.compare(sa[mid], min(n - sa[mid], m), pattern) <= 0) {
-					low = mid + 1;
-				} else {
-					high = mid - 1;
-				}
-			}
-			int last = high;
-			return max(0, last - first + 1);
-		}
-		long long distinct_substrings() {
-			long long total = (long long)n * (n + 1) / 2;
-			for (int i = 0; i < n - 1; i++) {
-				total -= lcp[i];
-			}
-			return total;
-		}
+	}
+	void build_sparse_table(int n) {
+		int lim = __lg(n);
+		st.resize(lim + 1, vector<int>(n)); st[0] = lcp;
+		for (int k = 1; k <= lim; k++)
+			for (int i = 0; i + (1 << k) <= n; i++)
+				st[k][i] = min(st[k - 1][i], st[k - 1][i + (1 << (k - 1))]);
+	}
+	int get_lcp(int i) { return lcp[i]; }
+	int get_lcp(int i, int j) {
+		if (j < i) swap(i, j);
+		j--; /*for lcp from i to j we don't need last lcp*/
+		int K = __lg(j - i + 1);
+		return min(st[K][i], st[K][j - (1 << K) + 1]);
+	}
 };
 
